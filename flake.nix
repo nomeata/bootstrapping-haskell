db2f92769786fec34637bdae1abea97aa194dffb
Joachim Breitner <mail@joachim-breitner.de>
2023-06-10 15:29:56 +0200

    Include hugs sources

--- flake.nix
+++ flake.nix
@@ -4,8 +4,16 @@
   description = "Bootstrappable GHC experiments";
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages."${system}"; in
+    let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      hugs = pkgs.hugs.overrideAttrs(old: {
+        src = ./hugs98-Sep2006;
+      });
+    in
     {
+      packages = {
+        inherit hugs;
+      };
       devShells.default = pkgs.mkShell {
         packages = [ pkgs.hugs pkgs.happy ];
       };
---
+++ shims/GlaExts.hs
@@ -0,0 +1,1 @@
+module GlaExts where
