32771451995de02d91beab1d040fe056caa99606
Joachim Breitner <mail@joachim-breitner.de>
2023-06-10 15:04:23 +0200

    Also add happy

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  description = "A very basic flake";
+  description = "Bootstrappable GHC experiments";
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let pkgs = nixpkgs.legacyPackages."${system}"; in
     {
       devShells.default = pkgs.mkShell {
-        packages = [ pkgs.hugs ];
+        packages = [ pkgs.hugs pkgs.happy ];
       };
     });
-}
+
